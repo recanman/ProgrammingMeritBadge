b4861c29c4d47791400b9474e38d6b1fab189683
B <b@Fantastics-MacBook-Pro.local>
2018-10-27 12:37:41 -0700

    Resolved conflicts with commits

--- convertTemp-3.7.10.py
+++ convertTemp-3.7.10.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
 
 def convertUSDtoEURO_req5():
@@ -164,6 +163,4 @@ print("This is the for the fourth requirement.")
 convertTemp_req4()
 print("This is for the final requirement.")
 convertUSDtoEURO_req5()
-=======
-
->>>>>>> 55369a79c33af44570af4a860c06f4f1319dae09
+print("Thank you!")
